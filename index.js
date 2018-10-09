6772f22063e8497560fff6f595599fab7f5574a3
Seth Thompson <s3th.thompson@gmail.com>
2018-10-09 12:13:30 -0400

    add 10/6/18

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(require('./stores/play'))
 app.route('/', require('./views/main'))
 app.route('/5-15-18', require('./views/5-15-18'))
 app.route('/9-1-18', require('./views/9-1-18'))
+app.route('/10-6-18', require('./views/10-6-18'))
 app.route('/*', require('./views/404'))
 
 module.exports = app.mount('body')
--- views/main.js
+++ views/main.js
@@ -17,8 +17,9 @@ function view (state, emit) {
         <header class="lg-psf lg-t0 lg-pt1">
           <h1 class="c-black"><a href="/" class="c-yellow tdn" target="_blank" rel="noopener noreferrer">Bloom Filter</a></h1>
           <h2 class="c-pink"><a href="https://soundcloud.com/bloomfilter"  target="_blank" rel="noopener noreferrer">Soundcloud</a>, <a href="https://twitter.com/s3ththompson" target="_blank" rel="noopener noreferrer">Twitter</a>, <a href="mailto:s3ththompson@gmail.com">Email</a></h2>
-          <h3><a href="/5-15-18">5/15/18 @ Partners, New Haven (Live)</a></h3>
+          <h3><a href="/10-6-18">10/6/18 @ Partners, New Haven (Live)</a></h3>
           <h3><a href="/9-1-18">9/1/18 @ Partners, New Haven (Live)</a></h3>
+          <h3><a href="/5-15-18">5/15/18 @ Partners, New Haven (Live)</a></h3>
         </header>
       </main>
     </body>
